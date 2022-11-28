992044ba8fb65d677666b234c2c3368de0322559
Oliulla <razasm29@gmail.com>
2022-11-29 02:57:59 +0600

    save advertise items in db and get it

--- index.js
+++ index.js
@@ -25,18 +25,16 @@ function run() {
       console.log("database connected");
 
       // db collections
-      const productsCollection = client
-        .db("alibris")
-        .collection("productCategories");
+      // const productsCollection = client
+      //   .db("alibris")
+      //   .collection("productCategories");
       const usersCollection = client.db("alibris").collection("users");
       const bookingsCollection = client.db("alibris").collection("bookings");
-      const sellerProductsCollection = client
-        .db("alibris")
-        .collection("sellerProducts");
-        const allProductsCollection = client.db('alibris').collection('products');
-
-      
-      
+      // const sellerProductsCollection = client
+      //   .db("alibris")
+      //   .collection("sellerProducts");
+      const allProductsCollection = client.db("alibris").collection("products");
+      const advertiseProductsCollection = client.db("alibris").collection("advertiseProducts");
 
       // send all users for admin
       app.get("/users", async (req, res) => {
@@ -91,6 +89,60 @@ function run() {
         }
       });
 
+      // send admin role
+      app.get("/users/admin/:email", async (req, res) => {
+        const email = req.params.email;
+        const query = { email };
+        const user = await usersCollection.findOne(query);
+        res.send({ isAdmin: user?.role === "admin" });
+      });
+
+      // send seller role
+      app.get("/users/seller/:email", async (req, res) => {
+        const email = req.params.email;
+        const query = { email };
+        const user = await usersCollection.findOne(query);
+        res.send({ isSeller: user?.role === "seller" });
+      });
+
+      // send buyer role
+      app.get("/users/buyer/:email", async (req, res) => {
+        const email = req.params.email;
+        const query = { email };
+        const user = await usersCollection.findOne(query);
+        res.send({ isBuyer: user?.role === "buyer" });
+      });
+
+      // send all sellers
+      app.get("/sellers", async (req, res) => {
+        const users = await usersCollection.find({}).toArray();
+        const sellers = users.filter((user) => user?.role === "seller");
+        res.send(sellers);
+      });
+
+      // delete seller
+      app.delete("/sellers/:id", async (req, res) => {
+        const id = req.params.id;
+        const query = { _id: ObjectId(id) };
+        const deleteSeller = await usersCollection.deleteOne(query);
+        res.send(deleteSeller);
+      });
+
+      // send all buyers
+      app.get("/buyers", async (req, res) => {
+        const users = await usersCollection.find({}).toArray();
+        const buyers = users.filter((user) => user?.role === "buyer");
+        res.send(buyers);
+      });
+
+      // delete buyer
+      app.delete("/buyers/:id", async (req, res) => {
+        const id = req.params.id;
+        const query = { _id: ObjectId(id) };
+        const deleteBuyer = await usersCollection.deleteOne(query);
+        res.send(deleteBuyer);
+      });
+
       // post users
       app.put("/users", async (req, res) => {
         try {
@@ -133,13 +185,12 @@ function run() {
           const product = req.body;
           const filter = {
             categoryName: product.categoryName,
-            email: product.email
           };
 
           const options = { upsert: true };
           const updatedDoc = {
             $push: {
-              products: product.products[0]
+              products: product.products[0],
             },
           };
 
@@ -226,14 +277,17 @@ function run() {
         try {
           const email = req.query?.email;
           const query = { email };
-          const myProducts = await allProductsCollection
-            .find(query)
-            .toArray();
-          if (myProducts) {
+          const allproducts = await allProductsCollection.find({}).toArray();
+
+          const myProd = allproducts.map((prod) =>
+            prod.products.filter((myprod) => myprod.email === email)
+          );
+
+          if (myProd) {
             res.json({
               status: true,
               message: "product got successfully",
-              data: myProducts,
+              data: myProd,
             });
           } else {
             res.json({
@@ -248,6 +302,55 @@ function run() {
           });
         }
       });
+
+      // advertise products send
+      app.get("/advertiseProducts", async(req, res) => {
+        const advertiseProducts = await advertiseProductsCollection.find({}).toArray();
+        res.send(advertiseProducts)
+      })
+
+      // advertise products save to db
+      app.post("/advertiseProducts", async(req, res) => {
+        const product = req.body;
+        // console.log(product)
+        const result = await advertiseProductsCollection.insertOne(product);
+        res.send(result)
+      })
+
+      
+
+      // delete products by sellers
+      // app.put("/myproducts/:id", async (req, res) => {
+      //   const deletedProduct = req.body;
+      //   // console.log(deletedProduct)
+
+      //   const id = req.params.id;
+      //   const query = { _id: ObjectId(id) };
+
+      //   const result = await allProductsCollection.findOne(query);
+
+      //   const remaining = result.products.filter(
+      //     (prod) => prod.bookName === deletedProduct.bookName
+      //   );
+      //   // console.log(result.products.includes(deletedProduct))
+      //   // const deletedProd = db.allProductsCollection.updateOne(query, {$pull: {products: {bookName: remaining[0].bookName}}})
+
+      //   const options = {upsert: false}
+
+      //   const updatedDoc = {
+      //     $pull: { products: { bookName: deletedProduct.bookName } }
+      //   }
+
+      //   const deletedResult = await allProductsCollection.updateOne(
+      //     query,
+      //     {
+      //       $pull: { products: { bookName: deletedProduct.bookName } }
+      //     }
+      //   );
+      //   res.send(deletedResult);
+      // });
+
+
 
       // send bookings based on user
       app.get("/bookings", async (req, res) => {
